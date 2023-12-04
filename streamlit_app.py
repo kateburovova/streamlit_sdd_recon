45524da2c9ebc3c5f0420e827d107bb0a196b436
Kate <kate.burovova@gmail.com>
2023-12-04 17:06:40 +0200

    test time

--- streamlit_app.py
+++ streamlit_app.py
@@ -91,8 +91,11 @@ df_orders_SDD_paid = crm_processing.get_paid_crm_orders(df_orders_SDD, start_dat
 
 total_sum_by_number_fin = recon.get_agg_fin_shipping_data(df_finance_sdd)
 st.dataframe(total_sum_by_number_fin)
-total_sum_by_number_crm = recon.get_agg_crm_shipping_data(df_orders_SDD_paid)
+st.write(len(total_sum_by_number_fin))
+# total_sum_by_number_crm = recon.get_agg_crm_shipping_data(df_orders_SDD_paid)
+total_sum_by_number_crm = df_orders_SDD_paid.groupby('clean_order_number')['discountTotal'].sum().reset_index()
 st.dataframe(total_sum_by_number_crm)
+st.write(len(total_sum_by_number_crm))
 
 # df_discounts_merged_nonzero = recon.get_discounts_mismatch(df_orders_SDD_paid, df_finance_sdd)
 st.write('get_discounts_mismatch done')
