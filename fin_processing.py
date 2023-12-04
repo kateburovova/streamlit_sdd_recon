48c7bec3987150ccacd1f961e6191d60fdb1f510
Kate <kate.burovova@gmail.com>
2023-12-04 09:11:41 +0200

    fixed caching

--- fin_processing.py
+++ fin_processing.py
@@ -10,7 +10,7 @@ from google.oauth2.service_account import Credentials
 import gspread
 
 
-# @st.cache
+@st.cache
 def get_df_from_google_spreadsheet(url, creds, sheet_name):
   """
   Extracts and returns a flattened list of unique values from a DataFrame column that contains lists.
@@ -42,15 +42,17 @@ def get_df_from_google_spreadsheet(url, creds, sheet_name):
   sheet = spreadsheet.worksheet(sheet_name)
   data = sheet.get_all_values()
   df = pd.DataFrame(data)
-  df.columns = df.iloc[0]
-  df = df.drop(0)
-  df.reset_index(inplace=True, drop=True)
+  # df.columns = df.iloc[0]
+  # df = df.drop(0)
+  # df.reset_index(inplace=True, drop=True)
 
   return df
 
 # @st.cache
 def format_fin_data(df):
-
+  df.columns = df.iloc[0]
+  df = df.drop(0)
+  df.reset_index(inplace=True, drop=True)
   # drop extra columns and empty rows
   df.drop(columns = ['порезка', ''], inplace=True)
   columns_to_check = ["Расход или доход", "Какая касса?", "Статья затрат", "Сумма (только цифры с разделителем - точкой)"]
